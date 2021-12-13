f6624b3654db562697211934bf45977aaac127a6
amerlin <merlin.andrea@gmail.com>
2021-12-13 16:35:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import 'react-calendar/dist/Calendar.css';
+import './aalto.css';
+import React, { useEffect, useState } from 'react';
+import Calendar from 'react-calendar';
+import dateEventi from './data.json';
+import moment from 'moment';
 
 function App() {
+  const [value, onChange] = useState(new Date());
+
+  function getDates (startDate, endDate) {
+    const dates = []
+    let currentDate = startDate
+    const addDays = function (days) {
+      const date = new Date(this.valueOf())
+      date.setDate(date.getDate() + days)
+      return date
+    }
+    while (currentDate <= endDate) {
+      dates.push(currentDate)
+      currentDate = addDays.call(currentDate, 1)
+    }
+    return dates
+  }
+
+  useEffect(()=>{
+       
+    let currentMonthDays = [];
+
+    for (var i = 0; i < dateEventi.length; i++) {
+      let currentDate = dateEventi[i];
+      let dateFrom = currentDate.from;
+      let dateTo = currentDate.to;
+
+      if (dateFrom === dateTo) {
+        let day = currentDate.format('DD');
+        currentMonthDays[day] = currentDate.title;
+      }
+      else {
+            let fData = moment.format(dateFrom, "YYYY-MM-DD");
+            let tData = moment.format(dateTo, "YYYY-MM-DD");
+            let dates = getDates(fData, tData);
+            for(var j=0;j<dates.length;j++){
+              let day = currentDate.format('DD');
+              currentMonthDays[day] = currentDate.title;
+            }
+      }
+    }
+
+  },[]);
+
+  function tileContent({ date, view }) {
+    if (view === 'month') {
+      var currentDate = moment(date, 'YYYY/MM/DD');
+      let currentDay = currentDate.format('DD');
+      let currenMonth = currentDate.format('MM');
+      for (var i = 0; i < dateEventi.length; i++) {
+        var from = moment(dateEventi[i].from, '"YYYY-MM-DD"');
+        var ferie = from.format('DD');
+        var mese = from.format('MM');
+        if ((ferie === currentDay) && (mese === currenMonth)) {
+          console.log(dateEventi[i].from);
+          return <p>{dateEventi[i].title}</p>
+        }
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calendar
+        onChange={onChange}
+        value={value}
+        tileContent={tileContent}
+        tileClassName={"aalto"}
+      />
     </div>
   );
 }
---
+++ src/dataRequest.ts
@@ -0,0 +1,7 @@
+interface dataRequest {
+    id: number;
+    color: string
+    from: string;
+    to: string;
+    title: string;
+}
